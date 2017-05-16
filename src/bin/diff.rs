860c0a70047db2ebd07cb1b33c0dadbcaec0c16e
Michael Donovan <mdonovan159@gmail.com>
2017-05-16 13:17:10 -0500

    diff takes command line args (paths to text files)

--- src/bin/diff.rs
+++ src/bin/diff.rs
@@ -1,9 +1,30 @@
 extern crate libdiff;
 use libdiff::{build_lcs_table, print_diff};
+use std::io::{Read, BufReader, BufRead};
+use std::env;
+use std::fs::File;
 
-fn main() {  
-  let from = vec!["this", "is", "test"];
-        let to = vec!["this", "is", "a", "test"];
-        let table = build_lcs_table(&from, &to);
-        print_diff(&table, &from, &to, from.len(), to.len());
+
+fn main() {
+    let args: Vec<_> = env::args().collect();
+    if args.len() != 3 { panic!("diff requires two paths to text files as arguments");}
+
+    let file_a = File::open(&args[1]).unwrap();
+    let file_b = File::open(&args[2]).unwrap();
+
+    let lines_a = read_lines(BufReader::new(file_a));
+    let lines_b = read_lines(BufReader::new(file_b));
+
+    let table = build_lcs_table(&lines_a, &lines_b);
+    print_diff(&table, &lines_a, &lines_b, lines_a.len(), lines_b.len());
+}
+
+fn read_lines<R: Read>(reader: R) -> Vec<String> {
+    let mut result: Vec<String> = Vec::new();
+    let mut lines = BufReader::new(reader).lines();
+        
+    while let Some(Ok(line)) = lines.next() {
+        result.push(line.to_string());
+    }
+    result
 }
