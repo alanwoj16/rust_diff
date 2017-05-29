b2a5a89566cf8ec6ac2e638a78f8de16de521d71
Michael Donovan <mdonovan159@gmail.com>
2017-05-29 16:09:49 -0500

    Fix docs again

---
+++ docs/implementors/core/fmt/trait.Display.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["colored"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"colored/struct.ColoredString.html\" title=\"struct colored::ColoredString\">ColoredString</a>",];
+implementors["diff"] = ["impl&lt;'a, T:&nbsp;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"diff/enum.DiffItem.html\" title=\"enum diff::DiffItem\">DiffItem</a>&lt;'a, T&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a>,&nbsp;</span>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
