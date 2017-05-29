90f26f9c26030ba4aed9c5ea69e139e07fc0c03c
Michael Donovan <mdonovan159@gmail.com>
2017-05-29 16:02:43 -0500

    Docs for gh-pages

---
+++ doc/implementors/core/str/trait.FromStr.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["colored"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html\" title=\"trait core::str::FromStr\">FromStr</a> for <a class=\"enum\" href=\"colored/enum.Color.html\" title=\"enum colored::Color\">Color</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
